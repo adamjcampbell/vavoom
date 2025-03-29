10daa96d72d23d07a1c3d47b7ceb55d416ae3f8f
Adam Campbell <adam.92.ac@gmail.com>
2025-03-29 13:54:39 +1100

    Detail out the view

--- Vavoom/ContentView.swift
+++ Vavoom/ContentView.swift
@@ -4,17 +4,34 @@ import SwiftUI
 struct ContentView: View {
     @SharedReader(.animeQuote) var animeQuote
 
+    var animeName: String {
+        let name = animeQuote?.anime.name ?? "Example Name"
+        let nameLine = (animeQuote?.anime.altName).map { "\(name) (\($0))" } ?? name
+        return "From: \(nameLine)"
+    }
     var quote: String {
-        animeQuote?.content ?? "This is a long placeholder that is shown when the quote is loading and should go over a couple lines."
+        animeQuote?.content ??
+            "This is a long placeholder that is shown when the quote is loading and should go over a couple lines."
+    }
+    var quotee: String {
+        let quotee = animeQuote?.character.name ?? "Example Name"
+        return "- \(quotee)"
     }
     var redactionReasons: RedactionReasons { animeQuote == nil ? .placeholder : [] }
 
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 0) {
             Text(quote)
+                .padding(.bottom, 4)
+            Text(quotee)
+                .font(.callout)
+                .padding(.bottom)
+            Text(animeName).font(.footnote)
         }
         .padding()
         .redacted(reason: redactionReasons)
+        .background(.background.secondary, in: RoundedRectangle(cornerRadius: 10))
+        .padding()
     }
 }
 
--- Vavoom/VavoomApp.swift
+++ Vavoom/VavoomApp.swift
@@ -1,48 +1,4 @@
-import Sharing
 import SwiftUI
-
-let rawJSON = #"{"status":"success","data":{"content":"Some things can't be prevented. The last of which, is death. All we can do is live until the day we die. Control what we can... and fly free!","anime":{"id":451,"name":"Space Brothers","altName":"Uchuu Kyoudai"},"character":{"id":1880,"name":"Deneil Young"}}}"#
-
-struct AnimeFactResponse: Decodable {
-    var status: String
-    var data: AnimeQuote
-}
-
-struct AnimeQuote: Decodable {
-    var content: String
-    var anime: Anime
-    var character: Character
-}
-
-struct Anime: Decodable {
-    var id: Int
-    var name: String
-    var altName: String?
-}
-
-struct Character: Decodable {
-    var id: Int
-    var name: String
-}
-
-struct AnimeQuoteKey: SharedReaderKey {
-    let id = UUID()
-
-    func load(context: Sharing.LoadContext<AnimeQuote?>, continuation: Sharing.LoadContinuation<AnimeQuote?>) {
-        let response = try! JSONDecoder().decode(AnimeFactResponse.self, from: rawJSON.data(using: .utf8)!)
-        continuation.resume(returning: response.data)
-    }
-
-    func subscribe(context: Sharing.LoadContext<AnimeQuote?>, subscriber: Sharing.SharedSubscriber<AnimeQuote?>) -> Sharing.SharedSubscription {
-        SharedSubscription {}
-    }
-}
-
-extension SharedReaderKey where Self == AnimeQuoteKey {
-    static var animeQuote: Self {
-        AnimeQuoteKey()
-    }
-}
 
 @main
 struct VavoomApp: App {
