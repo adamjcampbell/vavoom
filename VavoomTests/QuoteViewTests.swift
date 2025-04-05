6da767b47ffd8df5c83e4b71b10960f0abab8ff8
Adam Campbell <adam.92.ac@gmail.com>
2025-04-05 17:44:48 +1100

    Update the tests to use dependencies

--- VavoomTests/QuoteViewTests.swift
+++ VavoomTests/QuoteViewTests.swift
@@ -1,32 +1,54 @@
+import ConcurrencyExtras
+import Dependencies
+import DependenciesTestSupport
 import Foundation
 import SwiftUI
+import Synchronization
 import Testing
 @testable import Vavoom
 
+@Suite(
+    .dependencies {
+        $0.apiClient.getRandomQuote = { .spaceBrothers }
+    }
+)
 struct QuoteViewTests {
 
-    @Test func standardQuote() {
-        let quoteView = QuoteView(animeQuote: .init(ResultReaderKey<AnimeQuote?>(.spaceBrothers)))
+    @Test func standardQuote() async {
+        await withMainSerialExecutor {
+            let quoteView = await QuoteView()
 
-        #expect(quoteView.quote == "Some things can't be prevented. The last of which, is death. All we can do is live until the day we die. Control what we can... and fly free!")
-        #expect(quoteView.quotee == "- Deneil Young")
-        #expect(quoteView.source == "Space Brothers (Uchuu Kyoudai)")
-        #expect(quoteView.redactionReasons == [])
-        #expect(quoteView.errorConfiguration == nil)
+            await Task.yield()
+
+            await #expect(quoteView.quote == "Some things can't be prevented. The last of which, is death. All we can do is live until the day we die. Control what we can... and fly free!")
+            await #expect(quoteView.quotee == "- Deneil Young")
+            await #expect(quoteView.source == "Space Brothers (Uchuu Kyoudai)")
+            await #expect(quoteView.redactionReasons == [])
+            await #expect(quoteView.errorConfiguration == nil)
+        }
     }
 
-    @Test func duplicateNameQuote() {
-        let quoteView = QuoteView(animeQuote: .init(ResultReaderKey<AnimeQuote?>(.onePiece)))
+    @Test(
+        .dependencies {
+            $0.apiClient.getRandomQuote = { .onePiece }
+        }
+    )
+    func duplicateNameQuote() async {
+        await withMainSerialExecutor {
+            let quoteView = await QuoteView()
 
-        #expect(quoteView.quote == "It doesn't matter who your parents were. Everyone is a child of the sea.")
-        #expect(quoteView.quotee == "- Whitebeard")
-        #expect(quoteView.source == "One Piece")
-        #expect(quoteView.redactionReasons == [])
-        #expect(quoteView.errorConfiguration == nil)
+            await Task.yield()
+
+            await #expect(quoteView.quote == "It doesn't matter who your parents were. Everyone is a child of the sea.")
+            await #expect(quoteView.quotee == "- Whitebeard")
+            await #expect(quoteView.source == "One Piece")
+            await #expect(quoteView.redactionReasons == [])
+            await #expect(quoteView.errorConfiguration == nil)
+        }
     }
 
     @Test func loadingQuote() {
-        let quoteView = QuoteView(animeQuote: .init(ResultReaderKey<AnimeQuote?>()))
+        let quoteView = QuoteView(animeQuote: .init(value: nil))
 
         #expect(quoteView.quote == "This is a long placeholder that is shown when the quote is loading and should go over a couple lines.")
         #expect(quoteView.quotee == "- Example Name")
@@ -35,32 +57,59 @@ struct QuoteViewTests {
         #expect(quoteView.errorConfiguration == nil)
     }
 
-    @Test func errorQuote() {
-        let quoteView = QuoteView(animeQuote: .init(ResultReaderKey<AnimeQuote?>(throwing: NSError(domain: "", code: 0))))
+    @Test(
+        .dependencies {
+            $0.apiClient.getRandomQuote = { throw NSError(domain: "Test", code: 0) }
+        }
+    )
+    func errorQuote() async {
+        await withMainSerialExecutor {
+            let quoteView = await QuoteView()
 
-        #expect(quoteView.quote == "This is a long placeholder that is shown when the quote is loading and should go over a couple lines.")
-        #expect(quoteView.quotee == "- Example Name")
-        #expect(quoteView.source == "Example Name")
-        #expect(quoteView.redactionReasons == .placeholder)
-        #expect(quoteView.errorConfiguration == .init(title: "Failed to load quote", systemImageName: "text.quote", description: "Pull to refresh to try again"))
+            await Task.yield()
+
+            await #expect(quoteView.quote == "This is a long placeholder that is shown when the quote is loading and should go over a couple lines.")
+            await #expect(quoteView.quotee == "- Example Name")
+            await #expect(quoteView.source == "Example Name")
+            await #expect(quoteView.redactionReasons == .placeholder)
+            await #expect(quoteView.errorConfiguration == .init(title: "Failed to load quote", systemImageName: "text.quote", description: "Pull to refresh to try again"))
+        }
     }
 
     @Test func refreshingQuote() async throws {
-        let quoteView = QuoteView(animeQuote: .init(ResultReaderKey<AnimeQuote?>(.spaceBrothers)))
+        let mutex = Mutex(Optional<CheckedContinuation<AnimeQuote, any Error>>.none)
 
-        await #expect(quoteView.quote == "Some things can't be prevented. The last of which, is death. All we can do is live until the day we die. Control what we can... and fly free!")
-        await #expect(quoteView.quotee == "- Deneil Young")
-        await #expect(quoteView.source == "Space Brothers (Uchuu Kyoudai)")
-        await #expect(quoteView.redactionReasons == [])
-        await #expect(quoteView.errorConfiguration == nil)
+        try await withDependencies {
+            $0.apiClient.getRandomQuote = {
+                try await withCheckedThrowingContinuation { c in mutex.withLock { $0 = c } }
+            }
+        } operation: {
+            try await withMainSerialExecutor {
+                let quoteView = await QuoteView()
+                await Task.yield()
 
-        try await quoteView.refresh(ResultReaderKey<AnimeQuote?>(.onePiece))
+                mutex.withLock { $0?.resume(returning: .spaceBrothers) }
+                await Task.yield()
 
-        await #expect(quoteView.quote == "It doesn't matter who your parents were. Everyone is a child of the sea.")
-        await #expect(quoteView.quotee == "- Whitebeard")
-        await #expect(quoteView.source == "One Piece")
-        await #expect(quoteView.redactionReasons == [])
-        await #expect(quoteView.errorConfiguration == nil)
+                await #expect(quoteView.quote == "Some things can't be prevented. The last of which, is death. All we can do is live until the day we die. Control what we can... and fly free!")
+                await #expect(quoteView.quotee == "- Deneil Young")
+                await #expect(quoteView.source == "Space Brothers (Uchuu Kyoudai)")
+                await #expect(quoteView.redactionReasons == [])
+                await #expect(quoteView.errorConfiguration == nil)
+
+                let refreshTask = Task { try await quoteView.refresh(.animeQuote) }
+                await Task.megaYield()
+
+                mutex.withLock { $0?.resume(returning: .onePiece) }
+                try await refreshTask.value
+
+                await #expect(quoteView.quote == "It doesn't matter who your parents were. Everyone is a child of the sea.")
+                await #expect(quoteView.quotee == "- Whitebeard")
+                await #expect(quoteView.source == "One Piece")
+                await #expect(quoteView.redactionReasons == [])
+                await #expect(quoteView.errorConfiguration == nil)
+            }
+        }
     }
 
 }
